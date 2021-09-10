acc89ac70d22091a8e243dd86a22fd0b3b0b1d06
Sergei <cer1990@gmail.com>
2021-09-10 12:21:32 +0300

    refactor logging tests

--- src/test/java/com/demo/springreactivedemo/unit/YamlPropertySourceFactoryTests.java
+++ src/test/java/com/demo/springreactivedemo/unit/YamlPropertySourceFactoryTests.java
@@ -1,6 +1,5 @@
 package com.demo.springreactivedemo.unit;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
@@ -14,16 +13,10 @@ import com.demo.springreactivedemo.config.YamlPropertySourceFactory;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.EncodedResource;
-
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
 
 class YamlPropertySourceFactoryTests {
 
@@ -95,48 +88,5 @@ class YamlPropertySourceFactoryTests {
         );
 
         assertThrows(IllegalStateException.class, executable);
-    }
-
-    @Test
-    void fileNotFoundWarnLoggingEnabled() throws IOException {
-        ListAppender<ILoggingEvent> appender = new ListAppender<>();
-        Logger testLogger = (Logger) LoggerFactory.getLogger(YamlPropertySourceFactory.class);
-        testLogger.addAppender(appender);
-        appender.start();
-
-        try {
-            defautlYamlPropertySourceFactory.createPropertySource(
-                "notexists.yml",
-                new EncodedResource(new ClassPathResource("notexists.yml"))
-            );
-        } catch(FileNotFoundException e) {}
-
-        assertThat(appender.list)
-            .extracting(ILoggingEvent::getFormattedMessage)
-            .containsExactly("class path resource [notexists.yml] does not exist");
-
-        appender.stop();
-        testLogger.detachAppender(appender);
-    }
-
-    @Test
-    void fileNotFoundWarnLoggingDisabled() throws IOException {
-        ListAppender<ILoggingEvent> appender = new ListAppender<>();
-        Logger testLogger = (Logger) LoggerFactory.getLogger(YamlPropertySourceFactory.class);
-        testLogger.setLevel(Level.OFF);
-        testLogger.addAppender(appender);
-        appender.start();
-
-        try {
-            defautlYamlPropertySourceFactory.createPropertySource(
-                "notexists.yml",
-                new EncodedResource(new ClassPathResource("notexists.yml"))
-            );
-        } catch(FileNotFoundException e) {}
-
-        assertEquals(0, appender.list.size());
-
-        appender.stop();
-        testLogger.detachAppender(appender);
     }
 }
